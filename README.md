81072ff230933b241ac85d918b1a9e6eef7215d2
NaderSalama <nadersalama666@gmail.com>
2022-08-05 18:21:29 +0200

    Deploy

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# -Twitter-Sentiment-Analysis
+ <h1 align="center"> Twitter Sentiment Analysis <h1/>
+<p><img src="Cover.jpg" width = '100%' height = '100%' align="center"></p>
+
+
+## Table of contents
+- [Project Description](#Description)
+- [Required Packages](#Packages)
+- [How To Run](#Run)
+
+## Project Description
+Sentiment analysis are essential to detect and understand customer feelings. Sentiment analysis can improve customer loyalty and retention through better service outcomes and customer experience.
+
+Sentiment analysis is an application of natural language processing (NLP) that reveals the emotional states in human speech or text -- in Our case, the speech and text that customers generate. Businesses can use machine-learning-based sentiment analysis software to examine this speech and text for positive or negative sentiment about the brand.
+
+## Required Packages:
+
+- Python
+- numpy
+- nltk
+- pickle
+- Sklearn
+- Re
+
+## How To Run:
+-> Use 
+
+>Python Sentiment Analysis.py 
+
+To run the project.
---
+++ Sentiment Analysis.py
@@ -0,0 +1,58 @@
+import nltk, re, string
+from nltk.corpus import stopwords, twitter_samples
+import numpy as np
+import pickle
+
+def process_tweet(tweet):
+    stemmer = nltk.PorterStemmer()
+    stopwords_english = stopwords.words('english')
+    tweet = re.sub(r'\$\w*', '', tweet)
+    tweet = re.sub(r'^RT[\s]+', '', tweet)
+    tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
+    tweet = re.sub(r'#', '', tweet)
+    tokenizer = nltk.TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+
+    tweets_clean = []
+    for word in tweet_tokens:
+        if (word not in stopwords_english and
+                word not in string.punctuation):
+            stem_word = stemmer.stem(word)  # stemming word
+            tweets_clean.append(stem_word)
+
+    return tweets_clean
+
+def build_freqs(tweets, ys):
+    yslist = np.squeeze(ys).tolist()
+    freqs = {}
+    for y, tweet in zip(yslist, tweets):
+        for word in process_tweet(tweet):
+            pair = (word, y)
+            if pair in freqs:
+                freqs[pair] += 1
+            else:
+                freqs[pair] = 1
+
+    return freqs
+
+# Test if the code works properly
+tweets = ['i am happy', 'i am tricked', 'i am sad', 'i am tired', 'i am tired']
+ys = [1, 0, 0, 0, 0]
+res = build_freqs(tweets, ys)
+print(res)
+
+# select the set of positive and negative tweets
+all_positive_tweets = twitter_samples.strings('positive_tweets.json')
+all_negative_tweets = twitter_samples.strings('negative_tweets.json')
+# Train and Test
+test_pos = all_positive_tweets[4000:]
+train_pos = all_positive_tweets[:4000]
+test_neg = all_negative_tweets[4000:]
+train_neg = all_negative_tweets[:4000]
+# Merge Train and Test vectors into one set.
+train_x = train_pos + train_neg
+test_x = test_pos + test_neg
+# positive and negative Labels
+train_y = np.append(np.ones((len(train_pos), 1)), np.zeros((len(train_neg), 1)), axis=0)
+test_y = np.append(np.ones((len(test_pos), 1)), np.zeros((len(test_neg), 1)), axis=0)
+freqs = build_freqs(train_x, train_y)
